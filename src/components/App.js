8a7b73e13234c8ac6e9fff9bf3485cb78662c144
Joseph <45326298+jjeepjoe@users.noreply.github.com>
2021-06-24 22:00:51 -0500

    adjusted the oval shapes for media moved the like settings to another class for variable use on border size.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import Name from "./Name.js";
 
 function App() {
   return (
     <div id="shapeContainer">
-      <div className="oval daddyOval"><Name name="Joseph" /></div>
-      <div className="oval mommyOval"><Name name="Aileen" /></div>
-      <div className="oval emilyOval"><Name name="Emily" /></div>
-      <div className="oval rosemarieOval"><Name name="Rosemarie" /></div>
-      <div className="oval joeyOval"><Name name="Joseph II" /></div>
+      <div className="oval daddyOval oval-shape">
+        <Name name="Joseph" />
+      </div>
+      <div className="oval mommyOval oval-shape">
+        <Name name="Aileen" />
+      </div>
+      <div className="oval emilyOval oval-shape">
+        <Name name="Emily" />
+      </div>
+      <div className="oval rosemarieOval oval-shape">
+        <Name name="Rosemarie" />
+      </div>
+      <div className="oval joeyOval oval-shape">
+        <Name name="Joseph II" />
+      </div>
     </div>
   );
 }

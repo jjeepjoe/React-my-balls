c27113917e2186be7a515382286fdb52eaada5c5
Joseph <45326298+jjeepjoe@users.noreply.github.com>
2021-06-24 21:19:48 -0500

    Initial CodeSandbox install

--- src/index.js
+++ src/index.js
@@ -1,32 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
 //VIDEO START
-const videoStart = document.getElementById('video-speed');
-const videoEnd = document.getElementById('video-end');
-videoStart.addEventListener('ended', ChangeVideo);
-
+const videoStart = document.getElementById("video-speed");
+const videoEnd = document.getElementById("video-end");
+videoStart.addEventListener("ended", ChangeVideo);
 
 function ChangeVideo() {
-    console.log("DONE?");
-    videoStart.removeEventListener('ended', ChangeVideo);
-    videoStart.classList.toggle('video-hide');
-    videoStart.classList.toggle('video-show');  //HIDE
-    videoEnd.classList.toggle('video-hide'); //UN-HIDE
-    videoEnd.classList.toggle('video-show');
-    videoEnd.play();
+  videoStart.removeEventListener("ended", ChangeVideo);
+  videoStart.classList.toggle("video-hide");
+  videoStart.classList.toggle("video-show"); //HIDE
+  videoEnd.classList.toggle("video-hide"); //UN-HIDE
+  videoEnd.classList.toggle("video-show");
+  videoEnd.play();
 }
 // ////////////////////////////////////////////////////////////////////////
 
-const mainContainer = document.getElementsByClassName('main-container')[0];
+const mainContainer = document.getElementsByClassName("main-container")[0];
 console.log(mainContainer);
-window.addEventListener('load', function (event) {
-    console.log('page is fully loaded');
-    mainContainer.classList.add('start-transition');
+window.addEventListener("load", function (event) {
+  console.log("page is fully loaded");
+  mainContainer.classList.add("start-transition");
 });
 
 //REACT JSX ACTION
-const domContainer = document.getElementById('root');
-// 
-ReactDOM.render( <App />, domContainer);
+const domContainer = document.getElementById("root");
+//
+ReactDOM.render(<App />, domContainer);
